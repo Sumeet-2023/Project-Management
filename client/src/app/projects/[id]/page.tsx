c61e1442533e55dc6fad6bcc7828199cedcac9c5
Sumeet Mourya <go69ciq@mytum.de>
2024-11-20 14:30:47 +0100

    sidebar project list

---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React, { useState } from 'react'
+
+type Props = {
+  params: { id: string };
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+  return (
+    <div>Project</div>
+  )
+}
+
+export default Project
