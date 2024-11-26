547d8737fa94817342b96e8ab1f43a6607f30faa
Sumeet Mourya <go69ciq@mytum.de>
2024-11-26 16:28:12 +0100

    search implement

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
+import searchRoutes from "./routes/searchRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/projects", projectRoutes);
+app.use("/search", searchRoutes);
 app.use("/tasks", taskRoutes);
 
 /* SERVER */
